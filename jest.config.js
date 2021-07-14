5af555d697e5f813966ef663a35fdb8a1429a565
ticklepoke <lee.nigel@hotmail.com>
2021-07-14 15:59:00 +0800

    test: Add unit tests
    Closes #10

--- jest.config.js
+++ jest.config.js
@@ -1,4 +1,8 @@
 module.exports = {
 	preset: 'ts-jest',
+	testMatch: ['**/__tests__/**/*.(test).ts', '**/?(*.)+(test).ts'],
 	testEnvironment: 'node',
+	coveragePathIgnorePatterns: ['/node_modules'],
+	collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+	collectCoverage: true,
 };
